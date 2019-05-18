47843bfed59df0a4076666eebe32cb2f74b69d71
Fauzi <fauzibinfaisal@gmail.com>
2019-05-18 09:09:53 +0700

    animate

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myObject: UIButton!
+    @IBOutlet weak var myObjectBounds: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        myObject.layer.cornerRadius = myObject.frame.width/2
+        myObjectBounds.layer.cornerRadius = myObjectBounds.frame.width/2
+        
+        
+        print(self.myObjectBounds.frame)
+        print(self.myObject.frame)
+
+
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func actionButton(_ sender: Any) {
+        UIView.animate(withDuration: 1){
+            self.myObject.frame = CGRect(x: 5, y: 5, width: 80, height: 80)
 
-}
+            print(self.myObject.frame)
+        }
+        
+        UIView.animate(withDuration: 2){
+            self.myObjectBounds.bounds = CGRect(x: 0, y: 0, width: 80, height: 80)
+            self.myObjectBounds.transform = CGAffineTransform(scaleX: 2, y: 3)
+            
+            print(self.myObjectBounds.frame)
+            }
+        }
+    
+    
+        
+        
+        
+//
+//        UIView.animate(withDuration: 0.5, delay: 0.3, options: [],
+//                       animations: {
+//                        self.myObject.center.y += self.view.bounds.width
+//        },
+//                       completion: nil
+//        )
+        
+    }
+    
+
+
 
